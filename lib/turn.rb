3331cdb4c92c1969d2181413c6c03fc62c90991d
Taylor Donlan <taylordonlan@gmail.com>
2017-11-14 20:52:57 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,33 @@
+
+
+board = Array.new
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+    # May need to account for other possibilities?
+  end
+
+end
+
